04d866f9afde1b3e9aabd7ca97d2e557d5c8c403
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-12 20:15:59 +0530

    [Shubham]Added: Enqueue operation in queue

---
+++ src/test/java/com/capgi/QueueTest.java
@@ -0,0 +1,23 @@
+package com.capgi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class QueueTest {
+	@Test
+	public void enqueueTest() {
+		Queue queue = new Queue();
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+		queue.enqueue(firstNode);
+		queue.enqueue(secondNode);
+		queue.enqueue(thirdNode);
+		queue.printQueue();
+
+		boolean result = queue.linkedList.getHead().equals(firstNode) && queue.linkedList.getTail().equals(thirdNode);
+		assertTrue(result);
+
+	}
+}
